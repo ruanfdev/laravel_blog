4193e11197e152d435782ee7d49d1b29f83d9164
ruanfdev <ruanfdev@proton.me>
2024-11-22 10:56:26 +0200

    setup blog project

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('users', UserController::class);
+Route::apiResource('posts', PostController::class);
+Route::apiResource('comments', CommentController::class);
